d7a9df74eda5eed3cd3b4e458e988baef5a23e78
cariberrii <ca.fillingham@gmail.com>
2023-11-30 20:18:14 +0000

    Adds JS fetchVideo and reorders script in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 console.log("This thing is linked");
 
 // Adding some comments
+
+function fetchVideo(a) {
+    var queryURL = "https://api.dailymotion.com/videos?ids=" + a;
+    
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+}
+
+fetchVideo(sunny);
+var sunny = destinationSun[0].video;
+console.log(sunny);
